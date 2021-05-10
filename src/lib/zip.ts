dc9c20519da314b0ff8d8b64d363922b132b8495
JueMiaoShen <757076908@qq.com>
2021-05-10 15:36:26 +0800

    change bug for：compress dir is not current path, __dirname replace with process.cwd()

--- src/lib/zip.ts
+++ src/lib/zip.ts
@@ -2,14 +2,13 @@ import {processArgvType} from "../model/Argv";
 import * as path from "path";
 import  * as dirTool from './dirTool';
 import dayjs from "dayjs";
-
 const fs = require('fs');
 const archiver = require('archiver');
 
 
 
 const getArgvData=(dataArray:Array<any>)=>{
-        console.log("command:> txwzip-cli name=fileName  dir=./dd/  out=publish version=正式")
+        console.log("命令行:> txwzip-cli name=fileName  dir=./dd/  out=publish version=正式")
         let processArgv:any={};
         for (let i in dataArray){
             let a=dataArray[i];
@@ -21,7 +20,7 @@ const getArgvData=(dataArray:Array<any>)=>{
 }
 
 const zip=(processArgv:processArgvType)=>{
- console.log(processArgv);
+
     if(!processArgv.name){
         console.error("请输入压缩后文件名，格式：txwzip-cli name=fileName  dir=./dd/  out=publish version=正式");
         return
@@ -38,57 +37,41 @@ const zip=(processArgv:processArgvType)=>{
 
     }
 
-    const resolve = (dir:any) => path.join(__dirname, dir);
+    const resolve = (dir:any) => path.join(process.cwd(), dir);
 
     //const publishPath = resolve("publish");
 
     const publishPath = resolve(processArgv.out);
+    const compressDir = resolve(processArgv.dir);
 
     dirTool.dirExists(publishPath)
         .then((r:any) => {
-            console.log("publish dir is  existed"+r);
+            console.log("publish dir is  existed"+ r);
+            console.log("compress dir is "+compressDir)
+            console.log("publish dir is "+ publishPath);
 
             const version=processArgv.version?processArgv.version:'';
-
-            console.log(version);
-
-            const zipName = (() => `${processArgv.out}/${processArgv.name+dayjs().format("YYYY年MM月DD日HH时mm分ss秒")}-` + version + '.zip')();
-
-            console.log(zipName)
-
+            const zipName = (() => `${publishPath}/${processArgv.name+dayjs().format("YYYY年MM月DD日HH时mm分ss秒")}-` + version + '.zip')();
             const output = fs.createWriteStream(zipName);
-
-            output.on('close', function () {
+                  output.on('close', function () {
                 console.log(archive.pointer() + ' total bytes');
                 console.log('archiver has been finalized and the output file descriptor has closed.');
             });
-
-            output.on('end', function () {
+                  output.on('end', function () {
                 console.log('Data has been drained');
             });
 
             const archive = archiver('zip', {
                 zlib: {level: 9} // Sets the compression level.
             });
-
-            archive.on('error', function (err:any) {
+                  archive.on('error', function (err:any) {
                 throw err;
             });
-
-            // pipe archive data to the file
-
-            archive.pipe(output);
-
+                  archive.pipe(output); // pipe archive data to the file
             // append files from a sub-directory, putting its contents at the root of archive
             // archive.directory(resolve("/dist/"), false);
-
-            console.log(resolve(`${processArgv.dir}`))
-
-
-            archive.directory(resolve(`${processArgv.dir}`), false);
-
-
-            archive.on('warning', function (err:any) {
+                  archive.directory(compressDir, false);
+                  archive.on('warning', function (err:any) {
                 if (err.code === 'ENOENT') {
                     // log warning
                 } else {
@@ -96,8 +79,7 @@ const zip=(processArgv:processArgvType)=>{
                     throw err;
                 }
             });
-
-            archive.finalize();
+                  archive.finalize();
 
         })
         .catch((err:any) => {
